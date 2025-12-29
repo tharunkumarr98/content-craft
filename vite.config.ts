0be25c97ca7e2476d3549d83040a60872e39f2ef
Tharun <tharunkumarr98@gmail.com>
2025-12-29 07:59:41 +0530

    Changed the redirect path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/content-craft/", // 👈 IMPORTANT: repo name
+  // base: "/content-craft/", // 👈 IMPORTANT: repo name
   server: {
     host: "::",
     port: 8080,
